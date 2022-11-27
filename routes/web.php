93342ad0fac959499f0c4e6ae35fc294ce62a2cc
Venia <vsollery@gmail.com>
2022-11-28 01:12:42 +0700

    add views login and registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Post;
-use App\Http\Controllers\PostController;
-use App\Models\Category;
 use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -57,6 +59,9 @@ Route::get('/categories', function(){
 
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
 
 // Route::get('/authors/{author:username}', function(User $author){
 //     return view('posts',[
