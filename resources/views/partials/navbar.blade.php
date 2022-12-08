e744b722c7efa620758856a436b84f26a07c6b1a
Venia <vsollery@gmail.com>
2022-12-08 23:40:59 +0700

    add login auth and middleware

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,30 +1,49 @@
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light container-fluid">
-        <a class="navbar-brand ml-5" href="/" >Blog It</a>
+    <a class="navbar-brand ml-5" href="/">Blog It</a>
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link {{ ($active === 'home') ? 'active' : ''}}" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ ($active === 'about') ? 'active' : ''}}" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ ($active === 'posts') ? 'active' : ''}}" href="/posts">Blog</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ ($active === 'categories') ? 'active' : ''}}" href="/categories">Categories</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link {{ ($active === 'home') ? 'active' : ''}}" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ ($active === 'about') ? 'active' : ''}}" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ ($active === 'posts') ? 'active' : ''}}" href="/posts">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link {{ ($active === 'categories') ? 'active' : ''}}" href="/categories">Categories</a>
+            </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto mr-5">
+            @auth
+            <ul>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Welcome back, {{ auth()->user()->name }}
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#"><i class="bi bi-clipboard-fill"></i> Dashboard</a>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-right"></i> Logout</button>
+                        </form>
+
+                    </div>
                 </li>
             </ul>
-            <ul class="navbar-nav ml-auto mr-5">
-                <li class="nav-item">
-                    <a href="/login" class="nav-link {{ ($active === 'login') ? 'active' : ''}}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-                </li>
-            </ul>
-        </div>
+            @else
+            <!-- //show when user have not logged in yet -->
+            <li class="nav-item">
+                <a href="/login" class="nav-link {{ ($active === 'login') ? 'active' : ''}}"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+            </li>
+            @endauth
+        </ul>
+    </div>
 </nav>
